12d6bd8f54b2b7aca9d2a6378a05cd16c4790223
Gonzalo Salomon <salomongonzalo8@gmail.com>
2022-09-12 17:38:31 -0300

    first React commit
    created a React project and some components to be worked on later

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Card from './components/Card'
+import Footer from './components/Footer'
+import { useState } from 'react'
+
+function App() {
+  const [cards, setCards] = useState([]);
+
+  return (
+    <div className="App col">
+      <header className='row'>
+        <Header />
+        <Menu />
+      </header>
+      <section className='board'>
+        {()=>{for (let card in cards){
+          return(
+            <Card data={card} />
+          )
+        }}}
+      </section>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
