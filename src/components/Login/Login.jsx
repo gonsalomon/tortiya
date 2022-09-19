0edff974f3371a5a1e8631287863e140b5de6f55
Leonardo200298 <leonardosanchez119@gmail.com>
2022-09-19 12:03:19 -0300

    Feat: delete bootstrap library

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import {useState } from 'react'
+import { useState } from 'react'
 
 export default function Login() {
   const [login, setLogin] = useState("Logueate");
   return (
     <div>
-    <li onClick={()=>{setLogin("Logueate")}}>Tengo cuenta</li>
-    <li onClick={()=>{setLogin("No-log")}}>No tengo cuenta</li>
-
-    
-
-</div>
+      <li onClick={() => { setLogin("Logueate") }}>Tengo cuenta</li>
+      <li onClick={() => { setLogin("No-log") }}>No tengo cuenta</li>
+    </div>
   )
 }
